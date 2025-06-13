216c091064436ecdaff1055dc4c0bffa47bac978
nikhilshakya07 <nikhilshakya1308@gmail.com>
2025-06-14 00:02:33 +0530

    Added Contactitem component

---
+++ src/components/Sidebar/ContactItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ContactItem = ({ contact, isSelected, onSelect }) => {
+  return (
+    <div
+      onClick={onSelect}
+      className={`contact-item ${isSelected ? 'contact-item--selected' : ''}`}
+    >
+      <div className={`contact-avatar ${contact.color}`}>
+        {contact.avatar}
+      </div>
+      <div className="contact-info">
+        <div className="contact-header">
+          <h3 className="contact-name">{contact.name}</h3>
+          <span className="contact-time">{contact.time}</span>
+        </div>
+        <p className="contact-message">{contact.lastMessage}</p>
+        {contact.status === 'Online' && (
+          <div className="contact-status">
+            <div className="status-dot"></div>
+            <span className="status-text">Online</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
